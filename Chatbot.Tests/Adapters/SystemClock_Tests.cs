b26e47c8dfd8af2fc033c48e8621ecb1a6a41443
Matthew Butt <matthew@matthewbutt.com>
2015-12-28 17:00:26 +0000

    Characterise SystemClock.
    - This is a very rudimentary characterisation. I think I'm happy just to
    check it doesn't return DateTime.MinValue.

---
+++ Chatbot.Tests/Adapters/SystemClock_Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Chatbot.Adapters;
+using NUnit.Framework;
+
+namespace Chatbot.Tests.Adapters
+{
+    [TestFixture]
+    public class SystemClock_Tests
+    {
+        private SystemClock _systemClock;
+
+        [SetUp]
+        public void SetUp() => _systemClock = new SystemClock();
+
+        [Test]
+        public void Tells_the_time() => Assert.That(_systemClock.Now, Is.GreaterThan(DateTime.MinValue));
+    }
+}
