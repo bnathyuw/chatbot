95d84af297e2bb15cf70ff003ef8b218fc88bc36
Matthew Butt <matthew@matthewbutt.com>
2015-12-28 16:50:47 +0000

    Characterise MessageStore.
    - Not much to test here yet, but this will give us the framework to test forthcoming behaviour.

---
+++ Chatbot.Tests/Adapters/MessageStore_WithNoMessages_Tests.cs
@@ -0,0 +1,17 @@
+﻿using Chatbot.Adapters;
+using NUnit.Framework;
+
+namespace Chatbot.Tests.Adapters
+{
+    [TestFixture]
+    public class MessageStore_WithNoMessages_Tests
+    {
+        private MessageStore _messageStore;
+
+        [SetUp]
+        public void SetUp() => _messageStore = new MessageStore();
+
+        [Test]
+        public void Counts_zero_messages() => Assert.That(_messageStore.CountMessages(), Is.EqualTo(0));
+    }
+}
