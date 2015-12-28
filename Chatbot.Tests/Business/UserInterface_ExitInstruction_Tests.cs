eeb51694c36285843b8c1379e75f8d3a286f1b01
Matthew Butt <matthew@matthewbutt.com>
2015-12-28 17:54:57 +0000

    Characterise Exit instruction behaviour of UserInterface.
    - A simple one, this: it just returns State.Exit.
    - Lack of other interaction is proved by the fact null can be passed in as
      all UserInterface's other dependencies.

---
+++ Chatbot.Tests/Business/UserInterface_ExitInstruction_Tests.cs
@@ -0,0 +1,25 @@
+﻿using Chatbot.Business;
+using NUnit.Framework;
+
+namespace Chatbot.Tests.Business
+{
+    [TestFixture]
+    public class UserInterface_ExitInstruction_Tests : IInstructionReader
+    {
+        private string _instruction;
+        private State _state;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _instruction = "exit";
+            var userInterface = new UserInterface(this, null, null, null, null);
+            _state = userInterface.ProcessInstruction();
+        }
+
+        [Test]
+        public void Returns_an_exit_state() => Assert.That(_state, Is.EqualTo(State.Exit));
+
+        public string ReadInstruction() => _instruction;
+    }
+}
