bdb987149e68a3a35b9f115fccdc5bc46a1d6b6f
Matthew Butt <matthew@matthewbutt.com>
2015-12-28 17:18:50 +0000

    Start characterising how UserInterface processes a Status instruction.
    - I'm using the self-shunt pattern here, rather than a mocking framework,
    as I like the discipline self-shunt enforces.

---
+++ Chatbot.Tests/UserInterface_StatusInstruction_Tests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Chatbot.Business;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace Chatbot.Tests
+{
+    [TestFixture]
+    public class UserInterface_StatusInstruction_Tests : IInstructionReader, IMessageDisplayer, IClock, IMessageCounter, IUserConnexionCounter
+    {
+        private string _instruction;
+        private IList<string> _actualMessages;
+        private UserInterface _userInterface;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _instruction = "status";
+            _actualMessages = new List<string>();
+            _userInterface = new UserInterface(this, this, this, this, this);
+            _userInterface.ProcessInstruction();
+        }
+
+        [Test]
+        public void Reports_status_ok() => Assert.That(_actualMessages, Does.Contain("Status: ok"));
+
+        public string ReadInstruction() => _instruction;
+
+        public void ShowMessage(string output) => _actualMessages.Add(output);
+
+        public DateTime Now { get; }
+
+        public int CountMessages() => 35;
+
+        public int CountUserConnexions() => 46;
+    }
+}
