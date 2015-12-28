956f51bf8d87293218dff4bfb805f5427ff6559a
Matthew Butt <matthew@matthewbutt.com>
2015-12-28 16:34:56 +0000

    Characterise ConsoleIo's InstructionReader behaviour.
    - This is effectively a mini inegration/adapter test against the Standard
    library's Console implementation.

---
+++ Chatbot.Tests/Adapters/ConsoleIo_InstructionReader_Tests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using Chatbot.Adapters;
+using Chatbot.Business;
+using NUnit.Framework;
+
+namespace Chatbot.Tests.Adapters
+{
+    [TestFixture]
+    public class ConsoleIo_InstructionReader_Tests : TextReader
+    {
+        private TextReader _stdIn;
+        private IInstructionReader _instructionReader;
+        private const string ExpectedInstruction = "Expected Instruction";
+
+        [SetUp]
+        public void SetUp()
+        {
+            ImpersonateStandardInput();
+            _instructionReader = new ConsoleIo();
+        }
+
+        private void ImpersonateStandardInput()
+        {
+            _stdIn = Console.In;
+            Console.SetIn(this);
+        }
+
+        [TearDown]
+        public void TearDown() => RestoreStandardInput();
+
+        private void RestoreStandardInput() => Console.SetIn(_stdIn);
+
+        [Test]
+        public void Reads_instruction_from_standard_input() => 
+            Assert.That(_instructionReader.ReadInstruction(), Is.EqualTo(ExpectedInstruction));
+
+        public override string ReadLine() => ExpectedInstruction;
+    }
+}
