39981afb98c35ee5dbc487e7635de6b20fbfcc25
Matthew Butt <matthew@matthewbutt.com>
2015-12-28 16:49:00 +0000

    Add missing file from previous commit.

---
+++ Chatbot.Tests/Adapters/ConsoleIo_MessageDisplayer_Tests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Chatbot.Adapters;
+using Chatbot.Business;
+using NUnit.Framework;
+
+namespace Chatbot.Tests.Adapters
+{
+    [TestFixture]
+    public class ConsoleIo_MessageDisplayer_Tests : TextWriter
+    {
+        private TextWriter _stdOut;
+        private IMessageDisplayer _messageDisplayer;
+        private string _actualMessage;
+        private const string ExpectedMessage = "Expected Message";
+
+        [SetUp]
+        public void SetUp()
+        {
+            ImpersonateStandardOut();
+            _messageDisplayer = new ConsoleIo();
+        }
+
+        private void ImpersonateStandardOut()
+        {
+            _stdOut = Console.Out;
+            Console.SetOut(this);
+        }
+
+        [TearDown]
+        public void TearDown() => RestoreStandardOut();
+
+        private void RestoreStandardOut() => Console.SetOut(_stdOut);
+
+        [Test]
+        public void Displays_message_on_standard_out()
+        {
+            _messageDisplayer.ShowMessage(ExpectedMessage);
+            Assert.That(_actualMessage, Is.EqualTo(ExpectedMessage));
+        }
+
+        public override void WriteLine(string value) => _actualMessage = value;
+
+        public override Encoding Encoding => Encoding.UTF8;
+    }
+}
