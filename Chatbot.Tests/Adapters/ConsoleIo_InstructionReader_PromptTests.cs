bf7b5102e60dbcd91bf2428a6db43121a4802cc3
Matthew Butt <matthew@matthewbutt.com>
2015-12-29 16:16:54 +0000

    This may fix the tests that are timing out on AppVeyor.

--- Chatbot.Tests/Adapters/ConsoleIo_InstructionReader_PromptTests.cs
+++ Chatbot.Tests/Adapters/ConsoleIo_InstructionReader_PromptTests.cs
@@ -7,14 +7,17 @@ using NUnit.Framework;
 namespace Chatbot.Tests.Adapters
 {
     [TestFixture]
-    public class ConsoleIo_InstructionReader_PromptTests : TextWriter { 
+    public class ConsoleIo_InstructionReader_PromptTests : TextWriter
+    {
         private TextWriter _stdOut;
         private string _actualPrompt;
+        private TextReader _stdIn;
 
         [SetUp]
         public void SetUp()
         {
             ImpersonateStandardOut();
+            StubStandardIn();
             var messageDisplayer = new ConsoleIo();
             messageDisplayer.ReadInstruction();
         }
@@ -25,10 +28,22 @@ namespace Chatbot.Tests.Adapters
             Console.SetOut(this);
         }
 
+        private void StubStandardIn()
+        {
+            _stdIn = Console.In;
+            Console.SetIn(new Reader());
+        }
+
         [TearDown]
-        public void TearDown() => RestoreStandardOut();
+        public void TearDown()
+        {
+            RestoreStandardOut();
+            RestoreStandardIn();
+        }
 
         private void RestoreStandardOut() => Console.SetOut(_stdOut);
+
+        private void RestoreStandardIn() => Console.SetIn(_stdIn);
 
         [Test]
         public void Prompts_for_input() => Assert.That(_actualPrompt, Is.EqualTo("> "));
@@ -36,5 +51,13 @@ namespace Chatbot.Tests.Adapters
         public override void Write(string value) => _actualPrompt = value;
 
         public override Encoding Encoding => Encoding.UTF8;
+
+        private class Reader : TextReader
+        {
+            public override string ReadLine()
+            {
+                return "";
+            }
+        }
     }
 }
