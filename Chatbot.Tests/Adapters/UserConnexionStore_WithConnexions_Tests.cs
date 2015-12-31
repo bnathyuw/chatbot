e249c74f2b43ad333ecc160c034f7e21e53becc8
Matthew Butt <matthew@matthewbutt.com>
2015-12-31 08:35:23 +0000

    Implement IFollowedUserRetriever in UserConnexionStore
    - Though this is not wired in yet.

---
+++ Chatbot.Tests/Adapters/UserConnexionStore_WithConnexions_Tests.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using Chatbot.Adapters;
+using NUnit.Framework;
+
+namespace Chatbot.Tests.Adapters
+{
+    [TestFixture]
+    public class UserConnexionStore_WithConnexions_Tests
+    {
+        private UserConnexionStore _userConnexionStore;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _userConnexionStore = new UserConnexionStore();
+
+            _userConnexionStore.SaveConnexion("Alice", "Bob");
+            _userConnexionStore.SaveConnexion("Alice", "Charlie");
+            _userConnexionStore.SaveConnexion("Charlie", "Bob");
+        }
+
+        [Test]
+        public void Counts_user_connexions() => Assert.That(_userConnexionStore.CountUserConnexions(), Is.EqualTo(3));
+
+        [TestCase("Alice", 2)]
+        [TestCase("Charlie", 1)]
+        public void Retrieves_all_connections_for_a_single_user(string user, int expectedConnexionCount) =>
+            Assert.That(_userConnexionStore.RetrieveFollowedUsers(user).Count(), Is.EqualTo(expectedConnexionCount));
+
+        [TestCase("Alice", "Bob")]
+        [TestCase("Alice", "Charlie")]
+        [TestCase("Charlie", "Bob")]
+        public void Retrieves_correct_followed_users(string follower, string followed) =>
+            Assert.That(_userConnexionStore.RetrieveFollowedUsers(follower), Does.Contain(followed));
+    }
+}
--- Chatbot/Adapters/UserConnexionStore.cs
+++ Chatbot/Adapters/UserConnexionStore.cs
@@ -1,9 +1,31 @@
-﻿using Chatbot.Business;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Chatbot.Business;
 
 namespace Chatbot.Adapters
 {
-    public class UserConnexionStore : IUserConnexionCounter
+    public class UserConnexionStore : IUserConnexionCounter, IFollowedUserRetriever
     {
-        public int CountUserConnexions() => 0;
+        private readonly ISet<UserConnexion> _userConnexions = new HashSet<UserConnexion>();
+
+        public int CountUserConnexions() => _userConnexions.Count;
+
+        public void SaveConnexion(string follower, string followed) =>
+            _userConnexions.Add(new UserConnexion(follower, followed));
+
+        public IEnumerable<string> RetrieveFollowedUsers(string user) =>
+            _userConnexions.Where(c => c.Follower == user).Select(c => c.Followed);
+
+        private struct UserConnexion
+        {
+            public string Follower { get; }
+            public string Followed { get; }
+
+            public UserConnexion(string follower, string followed)
+            {
+                Follower = follower;
+                Followed = followed;
+            }
+        }
     }
 }
